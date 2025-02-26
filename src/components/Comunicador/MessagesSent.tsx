a224539adb17e54d69037918b2e28cf9f61d220d
penafiel.erick.martin <penafiel.erick.martin@gmail.com>
2025-02-26 17:51:02 -0400

    Update

--- src/components/Comunicador/MessagesSent.tsx
+++ src/components/Comunicador/MessagesSent.tsx
@@ -20,66 +20,7 @@ interface Log {
 }
 
 export const MessagesSent = () => {
-	const [data, setData] = useState<Log[]>([]);
 	const [loading, setLoading] = useState<boolean>(false);
-	const { instanceId } = useContext(UserContext);
-	const [numberPhone, setNumberPhone] = useState<string>("");
-
-	const getNumberPhone = useCallback(async () => {
-		const rawInformation = await clientService.getInformation({ instanceId });
-		console.log(rawInformation);
-		const { formattedNumber } = rawInformation.me.data;
-
-		return formattedNumber;
-	}, [instanceId]);
-
-	const loadMoreLogs = useCallback(async () => {
-		if (loading) return;
-		setLoading(true);
-
-		const messagesRef = ref(
-			databaseRealtime,
-			`messagesSent/${numberPhone.replace(" ", "")}`
-		);
-
-		onValue(messagesRef, async (snapshot) => {
-			const messagesArray: Log[] = [];
-
-			if (snapshot.exists()) {
-				const messagesData = snapshot.val();
-
-				for (const id in messagesData) {
-					const logsRef = child(messagesRef, `${id}/logs`);
-
-					const logsQuery = query(logsRef, orderByChild("timestamp"));
-
-					const logsSnapshot = await get(logsQuery);
-					if (logsSnapshot.exists()) {
-						const logsData = logsSnapshot.val();
-
-						Object.values(logsData).forEach((log: any) => {
-							messagesArray.push(log);
-						});
-					}
-				}
-			}
-
-			messagesArray.sort((a, b) => b.timestamp - a.timestamp);
-
-			setData(messagesArray);
-			setLoading(false);
-		});
-	}, [loading]);
-
-	useEffect(() => {
-		getNumberPhone().then((number) => {
-			setNumberPhone(number);
-		});
-	}, [getNumberPhone, numberPhone, setNumberPhone]);
-
-	useEffect(() => {
-		loadMoreLogs();
-	}, [loadMoreLogs]);
 
 	return (
 		<div className=" bg-transparent text-black p-4 rounded-2xl h-[50vh] overflow-y-auto border-5 border-purple-700">
@@ -93,18 +34,19 @@ export const MessagesSent = () => {
 						<DotLoader />
 					</div>
 				) : (
-					data.map((log, index) => (
-						<div
-							key={log.timestamp}
-							className="border-b-1 border-black p-2 text-black font-extralight"
-						>
-							<p>
-								Enviado por {log.sentBy} a {log.phoneNumber}
-							</p>
-							<p>Estado: {log.status === "sent" ? "Enviado" : "No enviado"}</p>
-							<p>Fecha: {new Date(log.timestamp).toLocaleString()}</p>
-						</div>
-					))
+					// data.map((log, index) => (
+					// 	<div
+					// 		key={log.timestamp}
+					// 		className="border-b-1 border-black p-2 text-black font-extralight"
+					// 	>
+					// 		<p>
+					// 			Enviado por {log.sentBy} a {log.phoneNumber}
+					// 		</p>
+					// 		<p>Estado: {log.status === "sent" ? "Enviado" : "No enviado"}</p>
+					// 		<p>Fecha: {new Date(log.timestamp).toLocaleString()}</p>
+					// 	</div>
+					// )
+					<p>Update</p>
 				)}
 			</div>
 		</div>
