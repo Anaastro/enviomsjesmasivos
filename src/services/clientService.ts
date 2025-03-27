aac4e0327f6a4538d8925980cb0575d661d0111a
storatus <sebastian.glahn89@web.de>
2025-03-27 18:21:36 -0400

    Refactor code for improved readability and consistency across services

--- src/services/clientService.ts
+++ src/services/clientService.ts
@@ -1,102 +1,102 @@
 export const clientService = {
-	getInformation: async ({ instanceId }: { instanceId: string }) => {
-		const response = await fetch("/api/getInformation", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ instanceId }),
-		});
+  getInformation: async ({ instanceId }: { instanceId: string }) => {
+    const response = await fetch("/api/getInformation", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ instanceId }),
+    });
 
-		const data = await response.json();
-		return data;
-	},
+    const data = await response.json();
+    return data;
+  },
 
-	async getStatus({ instanceId }: { instanceId: string }) {
-		const response = await fetch("/api/instanceStatus", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ instanceId }),
-		});
-		return await response.json();
-	},
+  async getStatus({ instanceId }: { instanceId: string }) {
+    const response = await fetch("/api/instanceStatus", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ instanceId }),
+    });
+    return await response.json();
+  },
 
-	async getQr({ instanceId }: { instanceId: string }) {
-		const response = await fetch(`/api/getQr`, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ instanceId }),
-		});
+  async getQr({ instanceId }: { instanceId: string }) {
+    const response = await fetch(`/api/getQr`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ instanceId }),
+    });
 
-		const data = await response.json();
-		const { qrCode } = data;
+    const data = await response.json();
+    const { qrCode } = data;
 
-		return qrCode;
-	},
+    return qrCode;
+  },
 
-	async getStatusInstance({
-		instanceId,
-		waapiApiKey,
-	}: {
-		instanceId: string;
-		waapiApiKey: string;
-	}) {
-		const response = await fetch(
-			`https://waapi.app/api/v1/instances/${instanceId}/client/status`,
-			{
-				method: "GET",
-				headers: {
-					Authorization: `Bearer ${waapiApiKey}`,
-				},
-			}
-		);
+  async getStatusInstance({
+    instanceId,
+    waapiApiKey,
+  }: {
+    instanceId: string;
+    waapiApiKey: string;
+  }) {
+    const response = await fetch(
+      `https://waapi.app/api/v1/instances/${instanceId}/client/status`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${waapiApiKey}`,
+        },
+      }
+    );
 
-		return await response.json();
-	},
+    return await response.json();
+  },
 
-	async getQrCode({
-		waapiApiKey,
-		instanceId,
-	}: {
-		waapiApiKey: string;
-		instanceId: string;
-	}) {
-		const response = await fetch(
-			`https://waapi.app/api/v1/instances/${instanceId}/client/qr`,
-			{
-				method: "GET",
-				headers: {
-					Authorization: `Bearer ${waapiApiKey}`,
-				},
-			}
-		);
+  async getQrCode({
+    waapiApiKey,
+    instanceId,
+  }: {
+    waapiApiKey: string;
+    instanceId: string;
+  }) {
+    const response = await fetch(
+      `https://waapi.app/api/v1/instances/${instanceId}/client/qr`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${waapiApiKey}`,
+        },
+      }
+    );
 
-		return await response.json();
-	},
+    return await response.json();
+  },
 
-	async getClientInformation({
-		waapiApiKey,
-		instanceId,
-	}: {
-		waapiApiKey: string;
-		instanceId: string;
-	}) {
-		const instanceIdNumber = +instanceId;
+  async getClientInformation({
+    waapiApiKey,
+    instanceId,
+  }: {
+    waapiApiKey: string;
+    instanceId: string;
+  }) {
+    const instanceIdNumber = +instanceId;
 
-		const response = await fetch(
-			`https://waapi.app/api/v1/instances/${instanceIdNumber}/client/me`,
-			{
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-					Authorization: `Bearer ${waapiApiKey}`,
-				},
-			}
-		);
-		return await response.json();
-	},
+    const response = await fetch(
+      `https://waapi.app/api/v1/instances/${instanceIdNumber}/client/me`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${waapiApiKey}`,
+        },
+      }
+    );
+    return await response.json();
+  },
 };
--- src/services/contactService.ts
+++ src/services/contactService.ts
@@ -1,13 +1,13 @@
 import {
-	addDoc,
-	collection,
-	collectionGroup,
-	deleteDoc,
-	doc,
-	getDocs,
-	query,
-	setDoc,
-	where,
+  addDoc,
+  collection,
+  collectionGroup,
+  deleteDoc,
+  doc,
+  getDocs,
+  query,
+  setDoc,
+  where,
 } from "firebase/firestore";
 import { database } from "@/lib/utils/firebase";
 import { getAuth } from "firebase/auth";
@@ -17,100 +17,100 @@ import { Phone } from "@/lib/interfaces/phone";
 const auth = getAuth();
 
 export const contactService = {
-	async addContact(phone: Phone) {
-		const user = auth.currentUser;
-		if (!user) {
-			console.error("No hay usuario autenticado");
-			return;
-		}
+  async addContact(phone: Phone) {
+    const user = auth.currentUser;
+    if (!user) {
+      console.error("No hay usuario autenticado");
+      return;
+    }
 
-		const userId = user.uid;
-		const userContactsRef = doc(database, "contacts", userId);
+    const userId = user.uid;
+    const userContactsRef = doc(database, "contacts", userId);
 
-		await setDoc(userContactsRef, {
-			userId: userId,
-			createdAt: new Date(),
-		});
+    await setDoc(userContactsRef, {
+      userId: userId,
+      createdAt: new Date(),
+    });
 
-		try {
-			const phonesCollectionRef = collection(userContactsRef, "phones");
+    try {
+      const phonesCollectionRef = collection(userContactsRef, "phones");
 
-			await addDoc(phonesCollectionRef, {
-				id: phone.id,
-				name: phone.name,
-				phone: phone.phone,
-				addedAt: new Date(),
-			});
-		} catch (error) {
-			console.error("Error al añadir contacto:", error);
-		}
-	},
+      await addDoc(phonesCollectionRef, {
+        id: phone.id,
+        name: phone.name,
+        phone: phone.phone,
+        addedAt: new Date(),
+      });
+    } catch (error) {
+      console.error("Error al añadir contacto:", error);
+    }
+  },
 
-	async getContacts() {
-		const user = auth.currentUser;
-		if (!user) {
-			console.error("No hay usuario autenticado");
-			return;
-		}
-		const q = query(
-			collection(database, "contacts"),
-			where("userId", "==", user.uid)
-		);
+  async getContacts() {
+    const user = auth.currentUser;
+    if (!user) {
+      console.error("No hay usuario autenticado");
+      return;
+    }
+    const q = query(
+      collection(database, "contacts"),
+      where("userId", "==", user.uid)
+    );
 
-		try {
-			const querySnapshot = await getDocs(q);
-			const contacts: any[] = [];
+    try {
+      const querySnapshot = await getDocs(q);
+      const contacts: any[] = [];
 
-			querySnapshot.forEach((doc) => {
-				const data = doc.data();
-				if (data.userId === user.uid) {
-					contacts.push({
-						id: doc.id,
-						phone: data.phone,
-					});
-				}
-			});
+      querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        if (data.userId === user.uid) {
+          contacts.push({
+            id: doc.id,
+            phone: data.phone,
+          });
+        }
+      });
 
-			return contacts;
-		} catch (error) {
-			console.error("Error al obtener contactos:", error);
-		}
-	},
+      return contacts;
+    } catch (error) {
+      console.error("Error al obtener contactos:", error);
+    }
+  },
 
-	async updateContact(id: string, phone: string) {
-		const user = auth.currentUser;
-		if (!user) {
-			console.error("No hay usuario autenticado");
-			return;
-		}
-		const docRef = doc(database, "contacts", id);
+  async updateContact(id: string, phone: string) {
+    const user = auth.currentUser;
+    if (!user) {
+      console.error("No hay usuario autenticado");
+      return;
+    }
+    const docRef = doc(database, "contacts", id);
 
-		try {
-			await setDoc(docRef, { phone }, { merge: true });
-		} catch (error) {
-			console.error("Error al modificar contacto:", error);
-		}
-	},
+    try {
+      await setDoc(docRef, { phone }, { merge: true });
+    } catch (error) {
+      console.error("Error al modificar contacto:", error);
+    }
+  },
 
-	async deleteContact(userId: string, phoneId: string) {
-		try {
-			const phonesCollectionRef = query(
-				collection(database, "contacts", userId, "phones"),
-				where("id", "==", phoneId)
-			);
-			const querySnapshot = await getDocs(phonesCollectionRef);
+  async deleteContact(userId: string, phoneId: string) {
+    try {
+      const phonesCollectionRef = query(
+        collection(database, "contacts", userId, "phones"),
+        where("id", "==", phoneId)
+      );
+      const querySnapshot = await getDocs(phonesCollectionRef);
 
-			if (!querySnapshot.empty) {
-				const docRef = querySnapshot.docs[0].ref;
-				await deleteDoc(docRef);
-			} else {
-				console.error(
-					"No se encontró ningún documento en 'contacts' con el userId proporcionado."
-				);
-			}
-		} catch (error) {
-			console.error("Error al eliminar el teléfono:", error);
-			throw error;
-		}
-	},
+      if (!querySnapshot.empty) {
+        const docRef = querySnapshot.docs[0].ref;
+        await deleteDoc(docRef);
+      } else {
+        console.error(
+          "No se encontró ningún documento en 'contacts' con el userId proporcionado."
+        );
+      }
+    } catch (error) {
+      console.error("Error al eliminar el teléfono:", error);
+      throw error;
+    }
+  },
 };
