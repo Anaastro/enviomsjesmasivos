4225fbe4bc60d012adb1e97208fd5547e741137e
storatus <sebastian.glahn89@web.de>
2025-04-02 01:35:57 -0400

    refactor: uncomment getServerSideProps logic in usuarios dashboard page and clean up commented code

--- src/pages/dashboard/usuarios/index.tsx
+++ src/pages/dashboard/usuarios/index.tsx
@@ -7,7 +7,7 @@ import TableUsers from "@/components/Dashboard/TableUsers";
 import nookies from "nookies";
 import { GetServerSidePropsContext } from "next";
 import { auth } from "@/config/firebaseAdmin";
-import { cookies } from "next/headers";
+// import { cookies } from "next/headers";
 
 interface Code {
   id: string;
@@ -16,40 +16,39 @@ interface Code {
   createdAt: Date;
 }
 
-// export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
-//   try {
-//     const cookies = nookies.get(ctx);
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  try {
+    // const cookies = nookies.get(ctx);
 
-//     const verifiedIdTokenResult = await auth.verifyIdToken(cookies.token);
-//     const isAdmin = verifiedIdTokenResult.admin;
+    // const verifiedIdTokenResult = await auth.verifyIdToken(cookies.token);
+    // const isAdmin = verifiedIdTokenResult.admin;
 
-//     if (!isAdmin) {
-//       ctx.res.writeHead(302, { Location: "/" });
-//       ctx.res.end();
-//     }
+    // if (!isAdmin) {
+    //   ctx.res.writeHead(302, { Location: "/" });
+    //   ctx.res.end();
+    // }
 
-//     const listUsersResult = await auth.listUsers();
-//     const users = listUsersResult.users.map((userRecord) => ({
-//       uid: userRecord.uid,
-//       email: userRecord.email,
-//     }));
+    // const listUsersResult = await auth.listUsers();
+    // const users = listUsersResult.users.map((userRecord) => ({
+    //   uid: userRecord.uid,
+    //   email: userRecord.email,
+    // }));
+    const users: any = [];
 
-//     return {
-//       props: {
-//         users: users,
-//       },
-//     };
-//   } catch (err) {
-//     ctx.res.writeHead(302, { Location: "/" });
-//     ctx.res.end();
-//     return {
-//       props: {} as never,
-//     };
-//   }
-// };
+    return {
+      props: {
+        users: users,
+      },
+    };
+  } catch (err) {
+    return {
+      props: {} as never,
+    };
+  }
+};
 
 const DashboardPage = ({ users }: { users: any }) => {
-  // console.log(users, "users");
+  console.log(users, "users");
   // const cookiesStore = cookies
   // console.log(cookiesStore, "cookiesStore");
   // console.log(cookies);
